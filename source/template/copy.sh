a3785f944d02a46f4e0c25e3e95108264e882ca2
Janne Adolf <janne.adolf@inbo.be>
2025-10-03 20:28:37 +0200

    adapt documentation bash script

--- source/template/copy.sh
+++ source/template/copy.sh
@@ -1,28 +1,27 @@
-
-
 #!/bin/bash
 set -e
 
+# define vars
 TEMPLATE_REPO="https://github.com/janneadolf/test-template.git"
 NEW_REPO_NAME=${1:-copy-template}
 LOCAL_PATH=${2:-github/$NEW_REPO_NAME}
 #REMOTE_URL=${3:-https://github.com/janneadolf/$NEW_REPO_NAME.git}
 
-# Clone template without full history
+# clone template without full history
 git clone --depth=1 "$TEMPLATE_REPO" "$LOCAL_PATH"
 
+# go to new repo
 cd "$LOCAL_PATH"
 
-# Remove old Git history
+# remove old git history
 rm -rf .git
 
-# Update local copy
+# update local repo copy
 mv test-template.Rproj "$NEW_REPO_NAME.Rproj" # update name R proj
-rm -rf source/template
+rm -rf source/template # remove template files
 
-# create remote repo (github-cli)
+# create remote repo (using github-cli)
 #gh repo create "$NEW_REPO_NAME" --private
-
 
 # create new repo
 git init
@@ -32,6 +31,3 @@ git branch -M main
 #git remote add origin "$REMOTE_URL"
 #git push -u origin main
 
-
-# run script
-#bash github\test-template\test\copy.sh
